70d951df1624e2f820cb0390aebfabdbf3fab036
Henry Opara <henry.o@hotmail.co.uk>
2024-09-30 07:36:42 +0100

    Created controller class

--- src/main/java/com/maxoptra/test1/controller/DataReceiver.java
+++ src/main/java/com/maxoptra/test1/controller/DataReceiver.java
@@ -1,4 +1,25 @@
 package com.maxoptra.test1.controller;
 
+import com.maxoptra.test1.model.BankingDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
 public class DataReceiver {
+
+    @PostMapping("/submitdata")
+    public ResponseEntity<String> submit(@RequestBody BankingDetails userInput){
+
+    }
+
+    @GetMapping("/displaydata")
+    public ResponseEntity<String> display(){
+
+    }
+
+
 }
